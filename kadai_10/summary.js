db7f38dc228041781f1e8419f18c63ab40093e6f
RYO-KWT <151120371+RYO-KWT@users.noreply.github.com>
2024-07-22 18:21:43 +0900

    kadai_10提出

--- kadai_10/summary.js
+++ kadai_10/summary.js
@@ -4,7 +4,7 @@ $(function () {
     });
 
     $('#change-text').on('click', function () {
-        $('#target').text('こんばんは！')
+        $('#target').text('Hello!')
     });
     $('#fade-out').on('click',function(){
         $('#target').fadeOut()
